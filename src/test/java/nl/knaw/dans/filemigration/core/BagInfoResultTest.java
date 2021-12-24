73e7bea8937f9357adc1e12f9987dfae7557532a
jo-pol <jo-pol@users.noreply.github.com>
2021-12-24 11:57:35 +0100

    dotting some i-s

--- src/test/java/nl/knaw/dans/filemigration/core/BagInfoResultTest.java
+++ src/test/java/nl/knaw/dans/filemigration/core/BagInfoResultTest.java
@@ -16,28 +16,47 @@
 package nl.knaw.dans.filemigration.core;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import nl.knaw.dans.lib.dataverse.DataverseItemDeserializer;
 import nl.knaw.dans.lib.dataverse.MetadataFieldDeserializer;
 import nl.knaw.dans.lib.dataverse.ResultItemDeserializer;
 import nl.knaw.dans.lib.dataverse.model.dataset.MetadataField;
 import nl.knaw.dans.lib.dataverse.model.dataverse.DataverseItem;
+import nl.knaw.dans.lib.dataverse.model.file.FileMeta;
 import nl.knaw.dans.lib.dataverse.model.search.ResultItem;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BagInfoResultTest {
 
-  @Test
-  public void canParse() throws IOException {
-    ObjectMapper mapper = new ObjectMapper();
+  protected ObjectMapper mapper;
+  private static final Class<?> classUnderTest = BagInfoResult.class;
+
+  @BeforeEach
+  public void beforeEach() {
+    mapper = new ObjectMapper();
     SimpleModule module = new SimpleModule();
     module.addDeserializer(MetadataField.class, new MetadataFieldDeserializer());
     module.addDeserializer(DataverseItem.class, new DataverseItemDeserializer());
     module.addDeserializer(ResultItem.class, new ResultItemDeserializer(mapper));
     mapper.registerModule(module);
+  }
+
+  @Test
+  public void canParse() throws IOException {
     mapper.readValue(new File("src/test/resources/bag-info.json"), BagInfoEnvelope.class).getResult().getBagInfo();
+  }
+
+  @Test
+  public void canNotParseEmpty() throws IOException {
+    try {
+      mapper.readValue("", BagInfoEnvelope.class);
+    } catch (MismatchedInputException ignored) {}
   }
 }
